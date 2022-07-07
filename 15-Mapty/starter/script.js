3d28f848f290a4768e227ec0f69685b545d66374
Piotr Potrac <piotrpotrac@gmail.com>
2022-07-07 22:29:58 +0200

    New project MAPTY with a lefleat library

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,69 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+let map, mapEvent;
+///////
+// geolocation
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      //   console.log(`https://www.google.com/maps/@${latitude}.${longitude}`);
+
+      map = L.map('map').setView([`${latitude}`, `${longitude}`], 13);
+      //   console.log(map);
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      //Handling pics on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  //Clearing inputs
+  inputCadence.value =
+    inputDistance.value =
+    inputDuration.value =
+    inputElevation.value =
+      '';
+
+  //Display marker
+
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 50,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+
+    .setPopupContent('Bike')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
