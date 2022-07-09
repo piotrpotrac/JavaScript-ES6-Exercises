b531fdaef62bb5c91658fa532151f3022b151425
Piotr Potrac <piotrpotrac@gmail.com>
2022-07-09 23:51:33 +0200

    Implementation a promises and a fetch API methods.

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -31,7 +31,7 @@ const renderCountry = function (data, className = '') {
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
 };
-
+/* 
 const getCountryandNeighbour = function (country) {
   //AJAX call country 1
   const request = new XMLHttpRequest();
@@ -81,4 +81,30 @@ setTimeout(() => {
       }, 1000);
     }, 1000);
   }, 1000);
-}, 1000);
+}, 1000); */
+//////////////////////////////////
+// Promises
+const request = fetch('https://restcountries.com/v2/name/portugal');
+
+console.log(request);
+/* 
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(function (response) {
+      console.log(response);
+
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      renderCountry(data[0]);
+    });
+}; */
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryData('portugal');
