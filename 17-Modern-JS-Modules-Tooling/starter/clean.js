2df9cdcdb154b2c4520ea40546dbad25c9530e3b
Piotr Potrac <piotrpotrac@gmail.com>
2022-07-19 21:18:13 +0200

    Next section with lerning about Functions oriented programing adn data flow in this pradigm.

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,4 +1,6 @@
-var budget = [
+'strict mode';
+
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,58 +9,63 @@ var budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-var limits = {
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
+});
+
+const getLimit = user => spendingLimits?.[user] ?? 0;
+
+//Pure function
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'jonas'
+) {
+  if (!user);
+  const cleanUser = user.toLowerCase();
+
+  // const limit = spendingLimits[user] ? spendingLimits[user] : 0;
+
+  return value <= getLimit(cleanUser)
+    ? [...state, { value: -value, description, user: cleanUser }]
+    : state;
 };
 
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
-  user = user.toLowerCase();
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
 
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
-  } else {
-    lim = 0;
-  }
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  100,
+  'Going to movies 🍿',
+  'Matilda'
+);
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
 
-  if (value <= lim) {
-    budget.push({ value: -value, description: description, user: user });
-  }
-};
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
-console.log(budget);
+console.log(newBudget3);
 
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
-    } else {
-      lim = 0;
-    }
-
-    if (el.value < -lim) {
-      el.flag = 'limit';
-    }
+const checkExpenses = function () {
+  for (const entry of budget) {
+    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
   }
 };
-check();
+checkExpenses();
 
-console.log(budget);
-
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
-    }
+const logBigExpenses = function (bigLimit) {
+  let output = '';
+  for (const entries of budget) {
+    output +=
+      entries.value <= -bigLimit ? `${entries.description.slice(-2)} / ` : '';
+    // Emojis are 2 chars
   }
   output = output.slice(0, -2); // Remove last '/ '
   console.log(output);
 };
+
+console.log(budget);
+logBigExpenses(500);
