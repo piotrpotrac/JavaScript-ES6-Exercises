0517762577a0fee948434a1ca19b95312f1695f1
Piotr Potrac <piotrpotrac@gmail.com>
2022-07-23 13:49:56 +0200

    Implementing a config.js and helper.js files and refactoring a addEventLiseners.

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -7,14 +7,6 @@ import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -36,11 +28,12 @@ const controlRecipes = async function () {
     console.log(model.state.recipe);
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err.message);
+    console.log(err.message);
   }
 };
 
 // controlRecipes();
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
